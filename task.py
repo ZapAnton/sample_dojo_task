c719b3db6427695053b6bece762e913ec013dfe6
ZapAnton <wc3ft2@gmail.com>
2019-10-15 13:40:07 +0300

    Refactored the task implementation to match ALGORITHM.md

--- task.py
+++ task.py
@@ -1,8 +1,15 @@
+from typing import Generator, Tuple
+
+
 def hamming_distance(strand_1: str, strand_2: str) -> int:
     if len(strand_1) != len(strand_2):
         raise ValueError('Input strands should be of equal length.')
+    symbol_pairs: Generator[Tuple[str, str], None, None] = (
+        (symbol_1, symbol_2)
+        for symbol_1, symbol_2 in zip(strand_1, strand_2)
+    )
     distance: int = sum(
         symbol_1 != symbol_2
-        for symbol_1, symbol_2 in zip(strand_1, strand_2)
+        for symbol_1, symbol_2 in symbol_pairs
     )
     return distance
